68e33ecc0e9f229cc4a1c19e8eb32daf97a7ec7b
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-07-13 22:55:18 +0700

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+
+// import BaiTapChonXeNangCao from "./BaiTapChonXeNangCao/BaiTapChonXeNangCao";
+import BaiTapThuKinh from "./BaiTapThuKinh/BaiTapThuKinh";
+// import BaiTapRenderFilms from "./BaiTapRenderFilms/BaiTapRenderFilms";
+// import BaiTapThucHanhLayout from "./component/BaiTapThucHanhLayOut/BaiTapThucHanhLayout";
+// import DateBinding from "./DateBinding/DateBinding";
+// import DateBindingRFC from "./DateBinding/DateBindingRFC";
+// import BaiTapChonXe from "./BaiTapChonXe/BaiTapChonXe";
+// import HandleEvent from "./HandleEvent/HandleEvent";
+import React from "react";
+
+// import RenderWithLoop from "./RenderWithLoop/RenderWithLoop";
+
+// import StateDemo from "./State/StateDemo";
+// import Styling from "./Styling/Styling";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <BaiTapThucHanhLayout /> */}
+      {/* <DateBinding />
+      <hr />
+      <DateBindingRFC /> */}
+      {/* <RenderingCondition /> */}
+      {/* <StateDemo /> */}
+      {/* <Styling/> */}
+      {/* <BaiTapChonXe/> */}
+      {/* <RenderWithLoop /> */}
+      {/* <BaiTapRenderFilms /> */}
+      <BaiTapThuKinh />/
+
+      {/* <BaiTapChonXeNangCao/> */}
+    </div>
+  );
+}
+
+export default App;
